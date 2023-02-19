2063ea07c488f9ad405a6bcbb35420895e9061f3
SonicSystemsAV <106649099+SonicSystemsAV@users.noreply.github.com>
2023-02-19 12:48:13 -0500

    key

--- src/openai.ts
+++ src/openai.ts
@@ -1,5 +1,10 @@
 import { ChatGPTAPI } from 'chatgpt'
 import { Configuration, OpenAIApi } from "openai";
+// Environment variables
+import dotenv from 'dotenv';
+
+// Load environment variables
+dotenv.config();
 
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
